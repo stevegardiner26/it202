2dd5f9910b169f0af54b52750f32d6ef928ddb18
Steve <stevegardiner26@aol.com>
2019-04-03 09:47:13 -0400

    Added Abiltiy to View Classes

--- process_form.php
+++ process_form.php
@@ -27,11 +27,46 @@ if($type == 'create') {
     $sql = "INSERT INTO patrons (`name`, `patron_id`, `email`, `schedule`) VALUES (`" . $name . "`,`" . $id . "`,`" . $email . "`,``)";
     $result = $conn->query($sql);
     echo "Congrats! You created an account with the user of " .$name. ", the id of " .$id. ",and the email of " .$email;
-    echo "<a href='assignment4.php'>Return to Form</a>";
+    echo "<br><a href='assignment4.php'>Return to Form</a>";
     $conn->close();
 } else {
     //Verify User
-
+    if($type == 'view') {
+        $sql = "SELECT * FROM patrons WHERE `patron_id` = " . $id;
+        $result = $conn->query($sql);
+        $result2 = $conn->query("SELECT * FROM courses WHERE `patron_id` = `" .$id."`");
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '
+                    <div style="margin: auto;">
+                        <h1>Patron Name: ' . $row['name'] . '</h1>
+                        <h2>Patron Id: ' . $row['patron_id'] . '</h2>
+                        <h2>Patron Email: ' . $row['email'] . '</h2>
+                    </div>
+                ';
+            }
+            if($result2->num_rows > 0) {
+                echo '<table class="table" style="border: 1px solid black;"><tr><th>Class Name</th><th>Trainer</th><th>Times</th></tr>';
+                while ($row2 = $result2->fetch_assoc()) {
+                    echo '
+                        <tr>
+                            <td>'.$row2['name'].'</td>
+                            <td>'.$row2['trainer'].'</td>
+                            <td>'.$row2['times'].'</td>
+                        </tr>
+                    ';
+                }
+                echo '</table>';
+            } else {
+                echo "You are not registered for any classes yet.";
+                echo "<br><a href='assignment4.php'>Return to Form</a>";
+            }
+        } else {
+            echo "Your Account is Invalid!";
+            echo "<br><a href='assignment4.php'>Return to Form</a>";
+        }
+    }
+    $conn->close();
 }
 
 /* vwhile($row = $result->fetch_assoc()) {
