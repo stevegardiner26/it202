99917cf723f813274b3178bcf628741adcf13b78
Steve <stevegardiner26@aol.com>
2019-04-03 10:27:04 -0400

    Recheck for Fix on Viewing

--- process_form.php
+++ process_form.php
@@ -31,7 +31,7 @@ if($type == 'create') {
     $conn->close();
 } else {
     //Verify User
-    $sql = "SELECT * FROM patrons WHERE `patron_id` = " . $id . ",`name` = " . $name;
+    $sql = "SELECT * FROM patrons WHERE `patron_id` = " . $id;
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
